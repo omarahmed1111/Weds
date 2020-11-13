cd997109a67357ea3af5badfc39116d5074aa012
Omar Ahmed <omarpirate2010@yahoo.com>
2020-11-13 15:19:15 +0200

    Restructure the repo folders

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./css/index.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
+  <React.StrictMode></React.StrictMode>,
   document.getElementById("root")
 );
 
