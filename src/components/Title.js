18a7be50b1ad5b799e0a04ae2e5279b5a5aae489
Omar Ahmed <omarpirate2010@yahoo.com>
2020-11-16 13:37:13 +0200

    Add title link to the all categories page

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 import {
   StyledTitle,
@@ -25,7 +26,9 @@ export default function Title(props) {
 
   return (
     <StyledTitle>
-      <TitleHeading>Gallery</TitleHeading>
+      <TitleHeading>
+        <Link to="/categories">Gallery</Link>
+      </TitleHeading>
       <TitleIcon className="fa fa-angle-right"></TitleIcon>
       <TitleHeading>{title}</TitleHeading>
     </StyledTitle>
